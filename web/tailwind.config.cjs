823a49aed53eee7774b9f694fcd61eac43aded41
Matheus Padilha Rodrigues <padilha.matheus@hotmail.com>
2022-09-13 18:54:21 -0300

    HighSpeed - class 2 LandingPage

---
+++ web/tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif']
+      },
+      backgroundImage:{
+        galaxy: "url('/bgGalaxy.png')",
+        'nlw-gradient':'linear-gradient(89.86deg, #8e57ba 20%, #8F90D7 40%, #60EDA9 60%, #CDA05C 80%)',    
+        'game-gradient':'linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.9) 67.88%)',
+      }
+    },
+  },
+  plugins: [],
+}
