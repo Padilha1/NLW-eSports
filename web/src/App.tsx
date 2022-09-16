4ba3fe04a2f73a14c5adce7ed82c31d16bfd675c
Matheus Padilha Rodrigues <padilha.matheus@hotmail.com>
2022-09-16 00:04:13 -0300

    Class 5 - Web finished

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,8 +7,8 @@ import { AdBanner } from './components/AdBanner'
 import * as Dialog from '@radix-ui/react-dialog'
 
 import { useState, useEffect } from 'react'
-import { GameController } from 'phosphor-react'
-import { Input } from './components/Form/Input'
+import { AdModal } from './components/AdModal'
+import axios from 'axios'
 
 interface Game {
 	id: string;
@@ -25,10 +25,8 @@ function App() {
 	const[ games, setGames] = useState<Game[]>([])
 
 	useEffect(()=> {
-		fetch('http://localhost:3333/games')
-		.then(response => response.json())
-		.then(data => {
-			setGames(data)
+		axios('http://localhost:3333/games').then(response => {
+			setGames(response.data)
 		})
 	}, 
 	[])
@@ -57,89 +55,8 @@ function App() {
 			<Dialog.Root>
 				<AdBanner/>
 
-				<Dialog.Portal>
-					<Dialog.Overlay className='bg-black/60 inset-0 fixed'/>
-
-					<Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25'>
-						<Dialog.Title className='text-3xl font-black'>Publish the Ad</Dialog.Title>
-							<form action="" className='mt-8 flex flex-col gap-4'>
-								<div className='flex flex-col gap-2'>
-									<label htmlFor="game" className='font-semibold'>Which game?</label>
-									<Input id="game"placeholder='Select the game you want to play'/>
-								</div>
-								<div className='flex flex-col gap-2'>
-									<label htmlFor="name">Your name or nickname</label>
-									<Input id="name" type="text" placeholder='How they call you ingame?'/>
-								</div>
-								<div className='grid grid-cols-2 gap-6'>
-									<div className='flex flex-col gap-2'>
-										<label htmlFor="yearsPlaying">Play how long ?</label>
-										<Input id="yearsPlaying" type="number"  placeholder='Alright to be 0'/>
-									</div>
-									<div className='flex flex-col gap-2'>
-										<label htmlFor="discord">What is your Discord?</label>
-										<Input id="discord" type="text" placeholder='User#3434'/>
-									</div>
-								</div>
-
-								<div className='flex gap-6'>
-									<div className='flex flex-col gap-2'>
-										<label htmlFor="weekDays">When do you play?</label>
-										<div className='grid grid-cols-4 gap-2'>
-											<button  className='w-8 h-8 rounded bg-zinc-900' 
-											title='Domingo'>D
-											</button>
-											<button  className='w-8 h-8 rounded bg-zinc-900' 
-											title='Segunda'>S
-											</button>
-											<button  className='w-8 h-8 rounded bg-zinc-900' 
-											title='Terca'>T</
-											button>
-											<button  className='w-8 h-8 rounded bg-zinc-900' 
-											title='Quarta'>Q
-											</button>
-											<button  className='w-8 h-8 rounded bg-zinc-900' 
-											title='Quinta'>Q
-											</button>
-											<button  className='w-8 h-8 rounded bg-zinc-900' 
-											title='Sexta'>S
-											</button>
-											<button  className='w-8 h-8 rounded bg-zinc-900' 
-											title='Sabado'>S
-											</button>
-										</div>
-
-									</div>
-									<div className='flex flex-col gap-2 flex-1'>
-										<label htmlFor="hourStart">What time?</label>
-										<div className='grid grid-cols-2 gap-2'>
-											<Input id="hourStart" type="time" placeholder='From'/>
-											<Input id="hourEnd" type="time" placeholder='Until'/>
-										</div>
-									</div>
-								</div>
-
-								<div className='mt-2 flex gap-2 text-sm'>
-									<input type="checkbox" />
-										I use VOIP
-								</div>
-								<footer className='mt-4 flex justify-end gap-4'>
-									<Dialog.Close type="button"
-											className='bg-zinc-500 rounded-md px-5 h-12 font-semibold hover:bg-zinc-600'>
-											Cancel
-									</Dialog.Close>
-									<button type='submit'
-											className='bg-violet-500 rounded-md px-5 h-12 font-semibold flex items-center gap-3 hover:bg-violet-700'
-									>
-										<GameController size={24}/>
-										Find Duo
-									</button>
-								</footer>
-							</form>
-					</Dialog.Content>
-				</Dialog.Portal>
+				<AdModal/>
 			</Dialog.Root>
-
 		</div>
   	)
 }
